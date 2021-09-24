7d13351807eccbb4758aabec174287bfa18e0887
isaac7 <isaac.arias07@gmail.com>
2021-09-24 01:36:55 -0600

    Reduciendo lineas de codigo por pagina dinamica

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,9 @@
+// Start Functions Load Components with index.html //
+
+async function loadHeader() {
+    header.innerHTML = await (await fetch('./components/html/01_header.html')).text();
+}
+
 async function loadCoverPage() {
     content.innerHTML = await (await fetch('./components/html/02_cover_page.html')).text();
 }
@@ -14,4 +20,17 @@ async function loadContact() {
     content.innerHTML = await (await fetch('./components/html/06_contact.html')).text();
 }
 
+async function loadFooter() {
+    footer.innerHTML = await (await fetch('./components/html/07_footer.html')).text();
+}
+
+// End Functions Load Components with index.html //
+
+// Start Load Functions default //
+loadHeader();
+
 loadCoverPage();
+
+loadFooter();
+
+// End Load Functions default //
