86cb001c3c21e064383553ccfeeed8e3bf5f9efc
isaac953 <isaac.arias07@gmail.com>
2021-10-07 18:02:45 -0600

    1) Rework en Header y Habilidades agregado al menu [07-10-2021]

--- portfolio.js
+++ portfolio.js
@@ -16,16 +16,20 @@ async function loadServices() {
     content.innerHTML = await (await fetch('./components/html/04_services.html')).text();
 }
 
+async function loadSkills() {
+    content.innerHTML = await (await fetch('./components/html/05_skills.html')).text();
+}
+
 async function loadProyects() {
-    content.innerHTML = await (await fetch('./components/html/05_proyects.html')).text();
+    content.innerHTML = await (await fetch('./components/html/06_proyects.html')).text();
 }
 
 async function loadContact() {
-    content.innerHTML = await (await fetch('./components/html/06_contact.html')).text();
+    content.innerHTML = await (await fetch('./components/html/07_contact.html')).text();
 }
 
 async function loadFooter() {
-    footer.innerHTML = await (await fetch('./components/html/07_footer.html')).text();
+    footer.innerHTML = await (await fetch('./components/html/08_footer.html')).text();
 }
 
 // End Functions Load Components with index.html //
