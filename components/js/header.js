64bcaa726104f489313ead15577ae727656973b3
isaac7 <isaac.arias07@gmail.com>
2021-10-12 02:24:45 -0600

    Header logo completo faltan las opciones del menu

--- components/js/header.js
+++ components/js/header.js
@@ -1,31 +1,33 @@
-let logoname = document.getElementById("logoname");
+const header = document.getElementById("header");
 
-logoname.style.display = "flex";
+header.style.width = "270px";
+
+// let logoname = document.getElementById("logoname");
+
+// logoname.style.display = "flex";
 
 function headerClick() {
+  const hamburger_buttom = document.getElementById("hamburger_buttom");
+  const header = document.getElementById("header");
+  const header__logo = document.getElementById("header__logo");
+  const logo_icon = document.getElementById("logo_icon");
+  const logoname = document.getElementById("logoname");
 
-    const hamburger_open = document.getElementById("hamburger_open");
-    const hamburger_close = document.getElementById("hamburger_close");
-    const header = document.getElementById("header");
-    const header__logo = document.getElementById("header__logo");
-    const logo_icon = document.getElementById("logo_icon");
-
-    if (logoname.style.display === "flex") {
-
-        header.style.width = "70px";
-        header.style.transition = "0.5s";
-        header__logo.style.height = "130px";
-        header__logo.style.flexDirection = "column";
-        logo_icon.style.minWidth = "70px";
-        logoname.style.display = "none";
-
-    } else {
-
-        header.style.width = "270px";
-        header.style.transition = "0.5s";
-        header__logo.style.height = "70px";
-        header__logo.style.flexDirection = "row-reverse";
-        logo_icon.style.minWidth = "205px";
-        logoname.style.display = "flex";
-    }
+  if (header.style.width === "270px") {
+    header.style.width = "70px";
+    header.style.transition = "0.8s";
+    header__logo.style.height = "130px";
+    header__logo.style.flexDirection = "column";
+    logo_icon.style.minWidth = "70px";
+    header__logo.style.paddingRight = "0px";
+    logoname.style.display = "none";
+  } else {
+    header.style.width = "270px";
+    header.style.transition = "0.8s";
+    header__logo.style.height = "70px";
+    header__logo.style.flexDirection = "row-reverse";
+    logo_icon.style.minWidth = "205px";
+    header__logo.style.paddingRight = "15px";
+    logoname.style.display = "flex";
+  }
 }
