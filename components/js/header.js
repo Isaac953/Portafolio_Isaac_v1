9d0b71a8650e3d821f701a6bef5d9c6a6748829b
isaac953 <isaac.arias07@gmail.com>
2021-10-12 19:51:45 -0600

    Sidebar en pantalla grande completo falta para pantallas de 850px y 650px

--- components/js/header.js
+++ components/js/header.js
@@ -1,60 +1,93 @@
-const sidebar = document.getElementById("sidebar");
+const sidebar = document.getElementById("sidebar").style;
 
-sidebar.style.width = "210px";
+sidebar.width = "210px";
 
 // let logoname = document.getElementById("logoname");
 
 // logoname.style.display = "flex";
 
 function headerClick() {
-  const sidebar = document.getElementById("sidebar");
-  const sidebarNavbar = document.getElementById("sidebar__navbar");
-  const aboutme = document.getElementById("aboutme");
-  const aboutme_link = document.getElementById("aboutme_link");
-  const services = document.getElementById("services");
-  const services_link = document.getElementById("services_link");
-  const skills = document.getElementById("skills");
-  const skills_link = document.getElementById("skills");
-  const proyects = document.getElementById("proyects");
-  const proyects_link = document.getElementById("proyects_link");
-  const contact = document.getElementById("contact");
-  const contact_link = document.getElementById("contact_link");
+  const sidebar = document.getElementById("sidebar").style;
+  const sidebarNavbar = document.getElementById("sidebar__navbar").style;
+  const aboutme = document.getElementById("aboutme").style;
+  const aboutme_link = document.getElementById("aboutme_link").style;
+  const services = document.getElementById("services").style;
+  const services_link = document.getElementById("services_link").style;
+  const skills = document.getElementById("skills").style;
+  const skills_link = document.getElementById("skills_link").style;
+  const proyects = document.getElementById("proyects").style;
+  const proyects_link = document.getElementById("proyects_link").style;
+  const contact = document.getElementById("contact").style;
+  const contact_link = document.getElementById("contact_link").style;
 
-  if (sidebar.style.width === "210px") {
-    sidebar.style.width = "70px";
-    sidebar.style.transition = "0.8s";
-    sidebarNavbar.style.width = "55px";
-    aboutme.style.display = "none";
-    aboutme.style.justifyContent = "center";
-    aboutme.style.minWidth = "55px";
-    services.style.display = "none";
-    services.style.minWidth = "55px";
-    skills.style.display = "none";
-    skills.style.minWidth = "55px";
-    proyects.style.display = "none";
-    proyects.style.minWidth = "55px";
-    contact.style.display = "none";
-    contact.style.minWidth = "55px";
+  if (sidebar.width === "210px") {
+    sidebar.width = "70px";
+    sidebar.transition = "0.8s";
+    sidebarNavbar.width = "55px";
+
+    aboutme.display = "none";
+    aboutme.justifyContent = "center";
+    aboutme.minWidth = "55px";
+    aboutme_link.minWidth = "55px";
+    aboutme_link.justifyContent = "center";
+
+    services.display = "none";
+    services.justifyContent = "center";
+    services.minWidth = "55px";
+    services_link.minWidth = "55px";
+    services_link.justifyContent = "center";
+
+    skills.display = "none";
+    skills.justifyContent = "center";
+    skills.minWidth = "55px";
+    skills_link.minWidth = "55px";
+    skills_link.justifyContent = "center";
+
+    proyects.display = "none";
+    proyects.justifyContent = "center";
+    proyects.minWidth = "55px";
+    proyects_link.minWidth = "55px";
+    proyects_link.justifyContent = "center";
+
+    contact.display = "none";
+    contact.justifyContent = "center";
+    contact.minWidth = "55px";
+    contact_link.minWidth = "55px";
+    contact_link.justifyContent = "center";
 
   } else {
-    sidebar.style.width = "210px";
-    sidebar.style.transition = "0.8s";
-    sidebarNavbar.style.width = "195px";
-    aboutme.style.display = "flex";
-    aboutme.style.justifyContent = "flex-start";
-    aboutme.style.minWidth = "140px";
-    services.style.display = "flex";
-    services.style.justifyContent = "flex-start";
-    services.style.minWidth = "140px";
-    skills.style.display = "flex";
-    skills.style.justifyContent = "flex-start";
-    skills.style.minWidth = "140px";
-    proyects.style.display = "flex";
-    proyects.style.justifyContent = "flex-start";
-    proyects.style.minWidth = "140px";
-    contact.style.display = "flex";
-    contact.style.justifyContent = "flex-start";
-    contact.style.minWidth = "140px";
+    sidebar.width = "210px";
+    sidebar.transition = "0.8s";
+    sidebarNavbar.width = "195px";
 
+    aboutme.display = "flex";
+    aboutme.justifyContent = "flex-start";
+    aboutme.minWidth = "140px";
+    aboutme_link.minWidth = "185px";
+    aboutme_link.justifyContent = "space-between";
+
+    services.display = "flex";
+    services.justifyContent = "flex-start";
+    services.minWidth = "140px";
+    services_link.minWidth = "185px";
+    services_link.justifyContent = "space-between";
+
+    skills.display = "flex";
+    skills.justifyContent = "flex-start";
+    skills.minWidth = "140px";
+    skills_link.minWidth = "185px";
+    skills_link.justifyContent = "space-between";
+
+    proyects.display = "flex";
+    proyects.justifyContent = "flex-start";
+    proyects.minWidth = "140px";
+    proyects_link.minWidth = "185px";
+    proyects_link.justifyContent = "space-between";
+
+    contact.display = "flex";
+    contact.justifyContent = "flex-start";
+    contact.minWidth = "140px";
+    contact_link.minWidth = "185px";
+    contact_link.justifyContent = "space-between";
   }
 }
