0a98503f1b17d739d9fd0f2ea617155f2f2d6e53
isaac7 <isaac.arias07@gmail.com>
2021-10-15 02:17:07 -0600

    Logica para menu mobile en proceso aun

--- components/js/header.js
+++ components/js/header.js
@@ -1,16 +1,11 @@
-// sidebar.width = "210px";
-
-// let logoname = document.getElementById("logoname");
-
-// logoname.style.display = "flex";
-
+//Function Normal NavBar
 function headerClick() {
 
   const sidebarValue = (document.getElementById("sidebar").offsetWidth).toString() + 'px';
 
   const sidebar = document.getElementById("sidebar").style;
   const sidebarExpanded = document.getElementById("sidebarExpanded").style;
-  const sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
+  const sidebarCollapsed = document.getElementById("sidebarCollapsed").style;  
 
   console.log(sidebarValue);
 
@@ -43,87 +38,25 @@ function headerClick() {
       sidebar.transition = "0.5s";
       console.log('Sidebar es de 65px');
       break;
-    // case 'none':
-    //   sidebar.height = "225px";
-    //   sidebarExpanded.display = "flex";
-    //   console.log('Sidebar es de 0px');
-    //   break;
-    // case '650px':
-    //   sidebar.height = "0px";
-    //   sidebarExpanded.display = "none";
-    //   console.log('Sidebar es de 180px');
-    //   break;
   }
-
-
-  // if (sidebar.width === "210px") {
-  //   sidebar.width = "70px";
-  //   sidebar.transition = "0.8s";
-  //   sidebarNavbar.width = "55px";
-
-  //   aboutme.display = "none";
-  //   aboutme.justifyContent = "center";
-  //   aboutme.minWidth = "55px";
-  //   aboutme_link.minWidth = "55px";
-  //   aboutme_link.justifyContent = "center";
-
-  //   services.display = "none";
-  //   services.justifyContent = "center";
-  //   services.minWidth = "55px";
-  //   services_link.minWidth = "55px";
-  //   services_link.justifyContent = "center";
-
-  //   skills.display = "none";
-  //   skills.justifyContent = "center";
-  //   skills.minWidth = "55px";
-  //   skills_link.minWidth = "55px";
-  //   skills_link.justifyContent = "center";
-
-  //   proyects.display = "none";
-  //   proyects.justifyContent = "center";
-  //   proyects.minWidth = "55px";
-  //   proyects_link.minWidth = "55px";
-  //   proyects_link.justifyContent = "center";
-
-  //   contact.display = "none";
-  //   contact.justifyContent = "center";
-  //   contact.minWidth = "55px";
-  //   contact_link.minWidth = "55px";
-  //   contact_link.justifyContent = "center";
-
-  // } else (sidebar.width === "70px") {
-  //   sidebar.width = "210px";
-  //   sidebar.transition = "0.8s";
-  //   sidebarNavbar.width = "195px";
-
-  //   aboutme.display = "flex";
-  //   aboutme.justifyContent = "flex-start";
-  //   aboutme.minWidth = "140px";
-  //   aboutme_link.minWidth = "185px";
-  //   aboutme_link.justifyContent = "space-between";
-
-  //   services.display = "flex";
-  //   services.justifyContent = "flex-start";
-  //   services.minWidth = "140px";
-  //   services_link.minWidth = "185px";
-  //   services_link.justifyContent = "space-between";
-
-  //   skills.display = "flex";
-  //   skills.justifyContent = "flex-start";
-  //   skills.minWidth = "140px";
-  //   skills_link.minWidth = "185px";
-  //   skills_link.justifyContent = "space-between";
-
-  //   proyects.display = "flex";
-  //   proyects.justifyContent = "flex-start";
-  //   proyects.minWidth = "140px";
-  //   proyects_link.minWidth = "185px";
-  //   proyects_link.justifyContent = "space-between";
-
-  //   contact.display = "flex";
-  //   contact.justifyContent = "flex-start";
-  //   contact.minWidth = "140px";
-  //   contact_link.minWidth = "185px";
-  //   contact_link.justifyContent = "space-between";
-  // }
 }
+
+//Function Mobile NavBar
+  function headerMobileClick() {
+    let navMobile = document.getElementById("navMobile");
+    let navMobileValue = navMobile.value;
+    let sidebar = document.getElementById("sidebar").style;
+    let sidebarExpanded = document.getElementById("sidebarExpanded").style;
+    let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;  
+
+    if(navMobileValue === "Collapsed"){
+      navMobile.value = "Expanded";
+      sidebar.display = "flex";
+      sidebar.transition = "0.5s";
+    }
+    else{
+      navMobile.value = "Collapsed";
+      sidebar.display = "none";
+      sidebar.transition = "0.5s";
+    }
+ }
