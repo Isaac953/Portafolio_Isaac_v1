f3d6c4a7635aa90ed7f5c175bc99a2dfa0089e95
isaac7 <isaac.arias07@gmail.com>
2021-10-17 02:23:37 -0600

    Funciones para controlar menu completado

--- components/js/header copy.js
+++ components/js/header copy.js
@@ -1,65 +1,141 @@
-//Function Normal NavBar
+
+let anchoVentana = window.innerWidth;
+
+let sidebar = document.getElementById("sidebar").style;
+// let sidebarExpanded = document.getElementById("sidebarExpanded").style;
+// let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
+
+const funcion1 = () => {
+  // console.log("Grande");
+
+  let screemSize = document.getElementById("screemSize");
+  screemSize.value = anchoVentana;
+
+  let navDesktop = document.getElementById("navDesktop");
+  navDesktop.value = "Expanded";
+
+  let sidebarExpanded = document.getElementById("sidebarExpanded").style;
+  let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
+
+  sidebar.width = "210px";
+  sidebar.height = "100vh";
+  sidebar.transition = "0s";
+  sidebarExpanded.display = "flex";
+  sidebarCollapsed.display = "none";
+
+}
+
+const funcion2 = () => {
+  // console.log("Mediana");
+
+  let screemSize = document.getElementById("screemSize");
+  screemSize.value = anchoVentana;
+
+  let navDesktop = document.getElementById("navDesktop");
+  navDesktop.value = "Expanded";
+
+  let sidebarExpanded = document.getElementById("sidebarExpanded").style;
+  let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
+
+  sidebar.width = "195px";
+  sidebar.height = "100vh";
+  sidebar.transition = "0s";
+  sidebarExpanded.display = "flex";
+  sidebarCollapsed.display = "none";
+}
+
+const funcion3 = () => {
+  // console.log("Pequeña");
+
+  let screemSize = document.getElementById("screemSize");
+  screemSize.value = anchoVentana;
+
+  let navMobile = document.getElementById("navMobile");
+  navMobile.value = "Collapsed";
+
+  let sidebarExpanded = document.getElementById("sidebarExpanded").style;
+  let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
+
+  sidebarExpanded.display = "none";
+  sidebarCollapsed.display = "none";
+  sidebar.transition = "0s";
+  sidebar.height = "0px";
+  sidebar.width = "100%";
+}
+
+window.onresize = function resize() {
+
+  // console.log(navMobileValue);
+
+  anchoVentana = window.innerWidth;
+  screemSize.value = anchoVentana;
+  // console.log(anchoVentana)
+  if (anchoVentana > 850) {
+    funcion1();
+  } else if (anchoVentana <= 850 && anchoVentana > 650) {
+    funcion2();
+  } else if (anchoVentana <= 650) {
+    funcion3();
+  }
+
+};
+
 function headerClick() {
 
-  const sidebarValue = (document.getElementById("sidebar").offsetWidth).toString() + 'px';
+  let navMobile = document.getElementById("navMobile");
+  let navMobileValue = navMobile.value;
+
+  let navDesktop = document.getElementById("navDesktop");
+  let navDesktopValue = navDesktop.value;
 
   const sidebar = document.getElementById("sidebar").style;
   const sidebarExpanded = document.getElementById("sidebarExpanded").style;
   const sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
 
   let screemSize = screen.width;
+
   console.log(screemSize);
+  console.log(navMobileValue);
 
-  console.log(sidebarValue);
-
-  switch (sidebarValue) {
-    case '210px':
-      sidebar.width = "70px";
-      sidebarExpanded.display = "none";
-      sidebarCollapsed.display = "flex";
-      sidebar.transition = "0.5s";
-      console.log('Sidebar es de 210px');
-      break;
-    case '70px':
-      sidebar.width = "210px";
-      sidebarExpanded.display = "flex";
-      sidebarCollapsed.display = "none";
-      sidebar.transition = "0.5s";
-      console.log('Sidebar es de 70px');
-      break;
-    case '195px':
-      sidebar.width = "65px";
-      sidebarExpanded.display = "none";
-      sidebarCollapsed.display = "flex";
-      sidebar.transition = "0.5s";
-      console.log('Sidebar es de 195px');
-      break;
-    case '65px':
-      sidebar.width = "195px";
-      sidebarExpanded.display = "flex";
-      sidebarCollapsed.display = "none";
-      sidebar.transition = "0.5s";
-      console.log('Sidebar es de 65px');
-      break;
+  if (screemSize >= 851 && navDesktopValue === "Expanded") {
+    navDesktop.value = "Collapsed";
+    sidebar.width = "70px";
+    sidebarExpanded.display = "none";
+    sidebarCollapsed.display = "flex";
+    sidebar.transition = "0.5s";
   }
-}
-
-//Function Mobile NavBar
-function headerMobileClick() {
-  let navMobile = document.getElementById("navMobile");
-  let navMobileValue = navMobile.value;
-  let sidebar = document.getElementById("sidebar").style;
-  let sidebarExpanded = document.getElementById("sidebarExpanded").style;
-  let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
-
-  if (navMobileValue === "Collapsed") {
+  else if (screemSize >= 851 && navDesktopValue === "Collapsed") {
+    navDesktop.value = "Expanded";
+    sidebar.width = "210px";
+    sidebarExpanded.display = "flex";
+    sidebarCollapsed.display = "none";
+    sidebar.transition = "0.5s";
+  }
+  else if (screemSize <= 850 && screemSize > 650 && navDesktopValue === "Expanded") {
+    navDesktop.value = "Collapsed";
+    sidebar.width = "65px";
+    sidebarExpanded.display = "none";
+    sidebarCollapsed.display = "flex";
+    sidebar.transition = "0.5s";
+  }
+  else if (screemSize <= 850 && screemSize > 650 && navDesktopValue === "Collapsed") {
+    navDesktop.value = "Expanded";
+    sidebar.width = "195px";
+    sidebarExpanded.display = "flex";
+    sidebarCollapsed.display = "none";
+    sidebar.transition = "0.5s";
+  }
+  else if (screemSize < 650 && navMobileValue === "Collapsed") {
     navMobile.value = "Expanded";
-    sidebar.display = "flex";
+    sidebarExpanded.display = "flex";
+    sidebar.height = "275px";
     sidebar.transition = "0.5s";
   }
-  else {
+  else if (screemSize < 650 && navMobileValue === "Expanded") {
     navMobile.value = "Collapsed";
-    sidebar.display = "none";
+    sidebarExpanded.display = "none";
+    sidebar.height = "0px";
     sidebar.transition = "0.5s";
   }
+
 }
--- components/js/header.js
+++ components/js/header.js
@@ -1,15 +1,11 @@
-
 let anchoVentana = window.innerWidth;
 
 let sidebar = document.getElementById("sidebar").style;
-// let sidebarExpanded = document.getElementById("sidebarExpanded").style;
-// let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
 
-const funcion1 = () => {
-  // console.log("Grande");
-
-  let screemSize = document.getElementById("screemSize");
-  screemSize.value = anchoVentana;
+//Funcion para pantalla Grande
+const largeScreen = () => {
+  let screenSize = document.getElementById("screenSize");
+  screenSize.value = anchoVentana;
 
   let navDesktop = document.getElementById("navDesktop");
   navDesktop.value = "Expanded";
@@ -17,19 +13,17 @@ const funcion1 = () => {
   let sidebarExpanded = document.getElementById("sidebarExpanded").style;
   let sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
 
-  sidebar.width = "210px";
+  sidebar.width = "275px";
   sidebar.height = "100vh";
   sidebar.transition = "0s";
   sidebarExpanded.display = "flex";
   sidebarCollapsed.display = "none";
+};
 
-}
-
-const funcion2 = () => {
-  // console.log("Mediana");
-
-  let screemSize = document.getElementById("screemSize");
-  screemSize.value = anchoVentana;
+//Funcion para pantalla Mediana
+const mediumScreem = () => {
+  let screenSize = document.getElementById("screenSize");
+  screenSize.value = anchoVentana;
 
   let navDesktop = document.getElementById("navDesktop");
   navDesktop.value = "Expanded";
@@ -42,13 +36,12 @@ const funcion2 = () => {
   sidebar.transition = "0s";
   sidebarExpanded.display = "flex";
   sidebarCollapsed.display = "none";
-}
+};
 
-const funcion3 = () => {
-  // console.log("Pequeña");
-
-  let screemSize = document.getElementById("screemSize");
-  screemSize.value = anchoVentana;
+//Funcion para pantalla Pequenia
+const smallScreem = () => {
+  let screenSize = document.getElementById("screenSize");
+  screenSize.value = anchoVentana;
 
   let navMobile = document.getElementById("navMobile");
   navMobile.value = "Collapsed";
@@ -61,81 +54,83 @@ const funcion3 = () => {
   sidebar.transition = "0s";
   sidebar.height = "0px";
   sidebar.width = "100%";
-}
-
-window.onresize = function resize() {
-
-  // console.log(navMobileValue);
-
-  anchoVentana = window.innerWidth;
-  screemSize.value = anchoVentana;
-  // console.log(anchoVentana)
-  if (anchoVentana > 850) {
-    funcion1();
-  } else if (anchoVentana <= 850 && anchoVentana > 650) {
-    funcion2();
-  } else if (anchoVentana <= 650) {
-    funcion3();
-  }
-
 };
 
-function headerClick() {
+//Funcion para ejecutarse al cambiar tamanio de pantalla
+window.onresize = function resize() {
+  anchoVentana = window.innerWidth;
+  screenSize.value = anchoVentana;
 
+  // console.log(screenSize.value);
+
+  if (anchoVentana > 850) {
+    largeScreen();
+  } else if (anchoVentana <= 850 && anchoVentana > 650) {
+    mediumScreem();
+  } else if (anchoVentana <= 650) {
+    smallScreem();
+  }
+};
+
+//Funcion para ejecutarse al hacer click en el boton de Hamburguesa para encoger y expandir menu
+function headerClick() {
   let navMobile = document.getElementById("navMobile");
   let navMobileValue = navMobile.value;
 
   let navDesktop = document.getElementById("navDesktop");
   let navDesktopValue = navDesktop.value;
 
-  const sidebar = document.getElementById("sidebar").style;
   const sidebarExpanded = document.getElementById("sidebarExpanded").style;
   const sidebarCollapsed = document.getElementById("sidebarCollapsed").style;
 
-  let screemSize = screen.width;
+  let screenSize = window.innerWidth;
 
-  console.log(screemSize);
-  console.log(navMobileValue);
+  let screenSizeInput = document.getElementById("screenSize");
+  screenSizeInput.value = screenSize;
 
-  if (screemSize >= 851 && navDesktopValue === "Expanded") {
+  // console.log(screenSize);
+
+  if (screenSize >= 851 && navDesktopValue === "Expanded") {
+    navDesktop.value = "Collapsed";
+    sidebar.width = "75px";
+    sidebarExpanded.display = "none";
+    sidebarCollapsed.display = "flex";
+    sidebar.transition = "0.5s";
+  } else if (screenSize >= 851 && navDesktopValue === "Collapsed") {
+    navDesktop.value = "Expanded";
+    sidebar.width = "275px";
+    sidebarExpanded.display = "flex";
+    sidebarCollapsed.display = "none";
+    sidebar.transition = "0.5s";
+  } else if (
+    screenSize <= 850 &&
+    screenSize > 650 &&
+    navDesktopValue === "Expanded"
+  ) {
     navDesktop.value = "Collapsed";
     sidebar.width = "70px";
     sidebarExpanded.display = "none";
     sidebarCollapsed.display = "flex";
     sidebar.transition = "0.5s";
-  }
-  else if (screemSize >= 851 && navDesktopValue === "Collapsed") {
+  } else if (
+    screenSize <= 850 &&
+    screenSize > 650 &&
+    navDesktopValue === "Collapsed"
+  ) {
     navDesktop.value = "Expanded";
-    sidebar.width = "210px";
+    sidebar.width = "250px";
     sidebarExpanded.display = "flex";
     sidebarCollapsed.display = "none";
     sidebar.transition = "0.5s";
-  }
-  else if (screemSize <= 850 && screemSize > 650 && navDesktopValue === "Expanded") {
-    navDesktop.value = "Collapsed";
-    sidebar.width = "65px";
-    sidebarExpanded.display = "none";
-    sidebarCollapsed.display = "flex";
-    sidebar.transition = "0.5s";
-  }
-  else if (screemSize <= 850 && screemSize > 650 && navDesktopValue === "Collapsed") {
-    navDesktop.value = "Expanded";
-    sidebar.width = "195px";
-    sidebarExpanded.display = "flex";
-    sidebarCollapsed.display = "none";
-    sidebar.transition = "0.5s";
-  }
-  else if (screemSize < 650 && navMobileValue === "Collapsed") {
+  } else if (screenSize < 650 && navMobileValue === "Collapsed") {
     navMobile.value = "Expanded";
     sidebarExpanded.display = "flex";
     sidebar.height = "275px";
     sidebar.transition = "0.5s";
-  }
-  else if (screemSize < 650 && navMobileValue === "Expanded") {
+  } else if (screenSize < 650 && navMobileValue === "Expanded") {
     navMobile.value = "Collapsed";
     sidebarExpanded.display = "none";
     sidebar.height = "0px";
     sidebar.transition = "0.5s";
   }
-
 }
