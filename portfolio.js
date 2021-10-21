ff182d04f0409b04ea97410dcaf779dc9984aafb
isaac7 <isaac.arias07@gmail.com>
2021-10-21 01:07:08 -0600

    Agregando imagen de fondo de forma dinamica

--- portfolio.js
+++ portfolio.js
@@ -1,36 +1,56 @@
 // Start Functions Load Components with index.html //
 
 const loadHeader = async () => {
-    header.innerHTML = await (await fetch('./components/html/01_header.html')).text();
-}
+  header.innerHTML = await (
+    await fetch("./components/html/01_header.html")
+  ).text();
+};
 
 const loadSidebar = async () => {
-    sidebar.innerHTML = await (await fetch('./components/html/02_sidebar.html')).text();
-}
+  sidebar.innerHTML = await (
+    await fetch("./components/html/02_sidebar.html")
+  ).text();
+};
 
+let loadCoverPage = async (mainContent, contentLayout) => {
+  content.innerHTML = await (
+    await fetch("./components/html/03_cover_page.html")
+  ).text();
+  mainContent.height = "120vh";
+  contentLayout.backgroundAttachment = "fixed";
+  contentLayout.backgroundImage = 'url("../../assets/cover_page_img.jpg")';
+  contentLayout.backgroundPosition = "center";
+  contentLayout.backgroundRepeat = "no-repeat";
+  contentLayout.backgroundSize = "cover";
+};
 
-let loadMaintenancePage = async (mainContent) => {
-    content.innerHTML = await (await fetch('./components/html/10_maintenance_page.html')).text();
-    mainContent.height = "120vh";
-}
+let loadMaintenancePage = async (mainContent, content) => {
+  content.innerHTML = await (
+    await fetch("./components/html/10_maintenance_page.html")
+  ).text();
+  mainContent.height = "120vh";
+};
 
 const loadFooter = async () => {
-    footer.innerHTML = await (await fetch('./components/html/09_footer.html')).text();
-}
+  footer.innerHTML = await (
+    await fetch("./components/html/09_footer.html")
+  ).text();
+};
 
 // End Functions Load Components with index.html //
 
 // Start Load Functions default //
 
 let loadPage = () => {
+  let mainContent = document.getElementById("mainContent").style;
+  let contentLayout = document.getElementById("content").style;
 
-    let mainContent = document.getElementById("mainContent").style;
-
-    loadHeader();
-    loadSidebar();
-    loadMaintenancePage(mainContent);
-    loadFooter();
-}
+  loadHeader();
+  loadSidebar();
+  //   loadMaintenancePage(mainContent);
+  loadCoverPage(mainContent, contentLayout);
+  loadFooter();
+};
 
 // loadHeader();
 
