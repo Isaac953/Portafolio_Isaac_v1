bb7d9ef9f9af52de996f9741bdebae540be6680c
isaac953 <isaac.arias07@gmail.com>
2021-11-24 17:22:18 -0600

    Proceso del carrousel

--- components/js/carousel.js
+++ components/js/carousel.js
@@ -1,21 +1,40 @@
-let next = () => {
+let next = async () => {
 
     let inputd = document.getElementById("inputd");
     let circles = document.getElementsByClassName("fas fa-circle");
     let circleslenght = circles.length;
     let inputint = parseInt(inputd.value);
 
+    let carouselitem = document.getElementsByClassName("carousel__item");
+
     if (inputint < circleslenght) {
         inputd.value = inputint + 1;
         for (let i = 0; i < circleslenght; i++) {
-            document.getElementsByClassName("fas fa-circle")[i].classList.remove("active");
+            circles[i].classList.remove("active");
+            carouselitem[i].classList.remove("default");
+            carouselitem[i].classList.remove("activeitem");
+
         }
-        document.getElementsByClassName("fas fa-circle")[inputint].classList.add("active");
+        circles[inputint].classList.add("active");
+        // carouselitem[inputint].classList.add("activeitem");
+
+        await setTimeout(() => {
+            console.log(inputint);
+            carouselitem[inputint].classList.add("activeitem");
+        }, 2000);
+
+
 
     } else {
         inputd.value = 1;
-        document.getElementsByClassName("fas fa-circle")[circleslenght - 1].classList.remove("active");
-        document.getElementsByClassName("fas fa-circle")[0].classList.add("active");
+        circles[circleslenght - 1].classList.remove("active");
+        carouselitem[circleslenght - 1].classList.remove("activeitem");
+        circles[0].classList.add("active");
+        carouselitem[0].classList.add("activeitem");
+
+        setTimeout(() => {
+            console.log(inputint);
+        }, 2000);
     }
 }
 
