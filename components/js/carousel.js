9b7db9b2b1128c624b643a6fa6d9f8316c9f9ca9
isaac953 <isaac.arias07@gmail.com>
2021-11-29 14:51:24 -0600

    Proyectos con dos carousel funcionando

--- components/js/carousel.js
+++ components/js/carousel.js
@@ -1,47 +1,90 @@
 let plusSlides = (n, carousel) => {
     let inputCarousel = document.getElementById("inputCarousel");
     inputCarousel.value = carousel;
-    showSlides((slideIndex += n));
+
+    showSlides((slideIndex = n));
 };
 
-let currentSlide = (n) => {
+let currentSlide = (n, carousel) => {
+    let inputCarousel = document.getElementById("inputCarousel");
+    inputCarousel.value = carousel;
     showSlides((slideIndex = n));
 };
 
 let showSlides = (n) => {
 
     try {
+        // let i;
+        // let carousel = document.getElementById("workCarousel-1");
+        // let slides = document.getElementsByClassName("slideshow__slide");
+        // let dots = document.getElementsByClassName("dot");
         let i;
-        let slides = document.getElementsByClassName("slideshow__slide");
-        let dots = document.getElementsByClassName("dot");
-        // switch (true) {
-        //     case inputCarousel.value === "carousel 1":
-        //         console.log(inputCarousel.value);
-        //         console.log(slides.length - 3);
-        //         console.log(slides);
-        //         break;
+        let slides;
+        let dots;
+        switch (true) {
+            case inputCarousel.value === "carousel 1":
+                slides = document.getElementById("workCarousel-1").getElementsByClassName("slideshow__slide");
+                dots = document.getElementById("workCarousel-1").getElementsByClassName("dot");
+                arrow = document.getElementById("workCarousel-1").getElementsByClassName("arrow");
 
-        //     case inputCarousel.value === "carousel 2":
-        //         console.log(inputCarousel.value);
-        //         break
+                if (n > slides.length) {
+                    slideIndex = 1;
+                }
+                if (n < 1) {
+                    slideIndex = slides.length;
+                }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].classList.remove("active__slide");
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].classList.remove("active");
+                    dots[i].classList.remove("active-dot");
+                }
+                slides[slideIndex - 1].classList.add("active__slide");
+                dots[slideIndex - 1].classList.add("active");
+
+
+                break;
+
+            case inputCarousel.value === "carousel 2":
+                slides = document.getElementById("workCarousel-2").getElementsByClassName("slideshow__slide");
+                dots = document.getElementById("workCarousel-2").getElementsByClassName("dot");
+                arrow = document.getElementById("workCarousel-1").getElementsByClassName("arrow");
+
+                if (n > slides.length) {
+                    slideIndex = 1;
+                }
+                if (n < 1) {
+                    slideIndex = slides.length;
+                }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].classList.remove("active__slide");
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].classList.remove("active");
+                    dots[i].classList.remove("active-dot");
+                }
+                slides[slideIndex - 1].classList.add("active__slide");
+                dots[slideIndex - 1].classList.add("active");
+                break
+        }
+
+
+        // if (n > slides.length) {
+        //     slideIndex = 1;
         // }
-
-
-        if (n > slides.length) {
-            slideIndex = 1;
-        }
-        if (n < 1) {
-            slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].classList.remove("active__slide");
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].classList.remove("active");
-            dots[i].classList.remove("active-dot");
-        }
-        slides[slideIndex - 1].classList.add("active__slide");
-        dots[slideIndex - 1].classList.add("active");
+        // if (n < 1) {
+        //     slideIndex = slides.length;
+        // }
+        // for (i = 0; i < slides.length; i++) {
+        //     slides[i].classList.remove("active__slide");
+        // }
+        // for (i = 0; i < dots.length; i++) {
+        //     dots[i].classList.remove("active");
+        //     dots[i].classList.remove("active-dot");
+        // }
+        // slides[slideIndex - 1].classList.add("active__slide");
+        // dots[slideIndex - 1].classList.add("active");
     }
     catch (e) {
 
